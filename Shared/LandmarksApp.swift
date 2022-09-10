14baf0f75bf56ac22e9978a9a9670a9ddf218ef2
Jeffrey Sun <telljsun@gmail.com>
2022-09-10 11:40:58 -0700

    Initial Commit

---
+++ Shared/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Shared
+//
+//  Created by Jeffrey Sun on 9/10/22.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
